3be5a3c2f8e64570235a06cc0691aebf21268568
hacking001 <hackingnulla@gmail.com>
2018-08-07 22:39:18 +0800

    清理 commits 历史

---
+++ README.md
@@ -0,0 +1,93 @@
+# v2ray 一键安装脚本 支持多种模式
+*有问题请发 [issue](https://github.com/hacking001/v2ray-installers/issues)*
+
+## 总览
+- [已通过测试的系统](#已通过测试的系统)
+- [注意事项](#注意事项)
+- [常用命令](#常用命令)
+- [准备工作](#准备工作)
+- [安装](#安装)
+    - [WebSockets for Debian、Ubuntu](#websockets-for-debianubuntu)
+    - [WebSockets for CentOS](#websockets-for-centos)
+    - [HTTP2 for Debian、Ubuntu](#http2-for-debianubuntu未经过测试)
+    - [HTTP2 for CentOS](#http2-for-centos)
+- [安装附带 BBRLKL 的版本](#安装附带-bbrlkl-的版本)
+    - [WebSockets for OpenVZ Debian、Ubuntu with BBRLKL](#websockets-for-openvz-debianubuntu-with-bbrlkl未经过测试)
+    - [WebSockets for OpenVZ CentOS with BBRLKL](#websockets-for-openvz-centos-with-bbrlkl未经过测试)
+- [内核更新](#内核更新)
+- [更新日志](#更新日志)
+    - [2018 年 8 月 6 日](#2018-年-8-月-6-日)
+
+## 已通过测试的系统
+- Ubuntu 16.04
+- CentOS 7
+
+## 注意事项
+- 不推荐 CentOS，因为可能需要自行放行端口
+- 请在干净的系统下安装
+- v2ray 依赖系统时间，请确保系统 UTC 时间误差在三分钟以内，时区无关
+- 脚本配置出来的 v2ray 默认屏蔽中国 IP、服务器内网段，这是为了安全
+- 与 Cloudflare 搭配的话请不要随便修改您的域名在 Cloudflare 上的设置（如果您不懂的话）
+- WebSockets 模式支持 Cloudflare 前置，默认端口安装即可（懂的请随意）
+
+## 常用命令
+检查 v2ray 状态
+```bash
+service v2ray status
+```
+启动 v2ray 服务
+```bash
+service v2ray start
+```
+关闭 v2ray 服务
+```bash
+service v2ray stop
+```
+重启 v2ray 服务
+```bash
+service v2ray restart
+```
+
+## 准备工作
+- 如果使用 HTTP2 模式，请准备一个域名，并设置 A 记录到服务器上
+
+## 安装
+### WebSockets for Debian、Ubuntu
+```bash
+wget -O WebSockets.sh https://raw.githubusercontent.com/hacking001/v2ray-installers/master/WebSockets.sh && bash WebSockets.sh
+```
+
+### WebSockets for CentOS
+```bash
+wget -O WebSockets.sh https://raw.githubusercontent.com/hacking001/v2ray-installers/master/WebSockets-CentOS.sh && bash WebSockets.sh
+```
+
+###  HTTP2 for Debian、Ubuntu（未经过测试）
+```bash
+wget -O HTTP2.sh https://raw.githubusercontent.com/hacking001/v2ray-installers/master/HTTP2.sh && bash HTTP2.sh
+```
+
+### HTTP2 for CentOS（未经过测试）
+```bash
+wget -O HTTP2.sh https://raw.githubusercontent.com/hacking001/v2ray-installers/master/HTTP2-CentOS.sh && bash HTTP2.sh
+```
+
+## 安装附带 BBRLKL 的版本
+### WebSockets for OpenVZ Debian、Ubuntu with BBRLKL（未经过测试）
+```
+wget -O WebSockets.sh https://raw.githubusercontent.com/hacking001/v2ray-installers/master/WebSockets-BBRLKL.sh && bash WebSockets.sh
+```
+### WebSockets for OpenVZ CentOS with BBRLKL（未经过测试）
+```
+wget -O WebSockets.sh https://raw.githubusercontent.com/hacking001/v2ray-installers/master/WebSockets-BBRLKL-CentOS.sh && bash WebSockets.sh
+```
+
+## 内核更新
+```
+wget -O v2ray-installer.sh https://install.direct/go.sh && bash v2ray-installer.sh && rm -f v2ray-installer.sh
+```
+来源：[下载安装 · Project V 官方网站](https://www.v2ray.com/chapter_00/install.html)
+
+## 更新日志
+### 2018 年 8 月 6 日
+- 项目初始化
---
+++ WebSockets-CentOS.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+GREEN="\033[32m"
+RED="\033[31m"
+GREENBG="\033[42;37m"
+REDBG="\033[41;37m"
+FONT="\033[0m"
+
+INFO="${GREEN}[信息]${FONT}"
+ERROR="${RED}[错误]${FONT}"
+
+V2RAY_CONFIG_PATH="/etc/v2ray"
+V2RAY_CONFIG="${V2RAY_CONFIG_PATH}/config.json"
+
+StatusEcho(){
+    if [[ $? -eq 0 ]]; then
+        echo -e "${INFO} ${GREENBG} $1 完成 ${FONT} "
+    else
+        echo -e "${ERROR} ${REDBG} $1 失败 ${FONT} "
+        exit 1
+    fi
+}
+
+echo -e "${INFO} Cloudflare 支持的端口列表："
+echo -e "${INFO} HTTP 协议：80、8080、8880、2052、2082、2086、2095"
+echo -e "${INFO} HTTPS 协议：443、2053、2083、2087、2096、8443"
+echo -e "${INFO} 在专业套餐及更高版本上，可以使用 WAF 规则 ID 100015 阻止除 80 和 443 之外的所有端口的请求"
+echo -e "${INFO} 80 和 443 端口是 Cloudflare Apps 能够使用的唯一端口"
+echo -e "${INFO} 80 和 443 端口是 Cloudflare Cache 能够使用的唯一端口"
+
+stty erase '^H' && read -p "请输入连接端口（默认：2082） => " PORT
+[[ -z ${PORT} ]] && PORT="2082"
+
+yum install wget -y
+StatusEcho "安装 WGET"
+yum install curl -y
+StatusEcho "安装 CURL"
+wget -O v2ray-installer.sh https://install.direct/go.sh
+StatusEcho "获取 V2RAY 安装脚本"
+bash v2ray-installer.sh --force
+StatusEcho "安装 V2RAY"
+
+UUID=$(cat /proc/sys/kernel/random/uuid)
+
+cat > ${V2RAY_CONFIG} << EOF
+{
+    "inbound": {
+EOF
+
+echo -e "        \"port\": ${PORT}," >> ${V2RAY_CONFIG}
+
+cat >> ${V2RAY_CONFIG} << EOF
+        "listen": "0.0.0.0",
+        "protocol": "vmess",
+        "settings": {
+            "clients": [
+                {
+EOF
+
+echo -e "                    \"id\":\"${UUID}\"," >> ${V2RAY_CONFIG}
+
+cat >> ${V2RAY_CONFIG} << EOF
+                    "alterId": 64
+                }
+            ]
+        },
+        "streamSettings": {
+            "network": "ws"
+        }
+    },
+    "outbound": {
+        "protocol": "freedom",
+        "settings": {}
+    },
+    "outboundDetour": [
+        {
+            "protocol": "freedom",
+            "settings": {},
+            "tag": "direct"
+        },
+        {
+            "protocol": "blackhole",
+            "settings": {
+                "response": {
+                    "type": "http"
+                }
+            },
+            "tag": "shield"
+        }
+    ],
+    "routing": {
+        "strategy": "rules",
+        "settings": {
+            "domainStrategy": "IPIfNonMatch",
+            "rules": [
+                {
+                    "type": "chinaip",
+                    "outboundTag": "shield"
+                },
+                {
+                    "type": "field",
+                    "ip": [
+                        "0.0.0.0/8",
+                        "10.0.0.0/8",
+                        "100.64.0.0/10",
+                        "127.0.0.0/8",
+                        "169.254.0.0/16",
+                        "172.16.0.0/12",
+                        "192.0.0.0/24",
+                        "192.0.2.0/24",
+                        "192.168.0.0/16",
+                        "198.18.0.0/15",
+                        "198.51.100.0/24",
+                        "203.0.114.0/24",
+                        "::1/128",
+                        "fc00::/7",
+                        "fe00::/10"
+                    ],
+                    "outboundTag": "shield"
+                }
+            ]
+        }
+    }
+}
+EOF
+
+service v2ray restart
+StatusEcho "V2RAY 加载配置"
+
+echo -e "${INFO} ${GREENBG} V2RAY WebSockets 安装成功！${FONT} "
+echo -e "${INFO} ${REDBG} 端口： ${FONT} ${PORT}"
+echo -e "${INFO} ${REDBG} ID： ${FONT} ${UUID}"
+
+rm -f v2ray-installer.sh > /dev/null 2>&1
+rm -f WebSockets.sh > /dev/null 2>&1
