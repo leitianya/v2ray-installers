d6da55af472662d8ca7b524d2c1121cda57f220f
Holli_Freed <x2360415889@outlook.com>
2018-12-14 19:14:30 +0800

    尝试修复 Unicode 报错

--- v2ray-installers.py
+++ v2ray-installers.py
@@ -82,28 +82,28 @@ def main():
 		pass
 	
 	print('1. TCP 2. WebSockets 3. mKCP')
-	print('请选择传输模式（输入序列号） => ', end = '')
+	print(u'请选择传输模式（输入序列号） => ', end = '')
 	CFG_MODE = input()
 	if CFG_MODE == 1:
-		print('您已选择 TCP 传输模式 ...')
+		print(u'您已选择 TCP 传输模式 ...')
 		defaultConf['inbounds'][0]['streamSettings']['network'] = 'tcp'
 	elif CFG_MODE == 2:
-		print('您已选择 WebSockets 传输模式 ...')
+		print(u'您已选择 WebSockets 传输模式 ...')
 		defaultConf['inbounds'][0]['streamSettings']['network'] = 'ws'
 	elif CFG_MODE == 3:
-		print('您已选择 mKCP 传输模式 ...')
+		print(u'您已选择 mKCP 传输模式 ...')
 		defaultConf['inbounds'][0]['streamSettings']['network'] = 'mkcp'
 	else:
-		print('选择有误 ...')
+		print(u'选择有误 ...')
 		return
 	
-	print('Cloudflare 支持的端口列表：')
-	print('HTTP 协议：80、8080、8880、2052、2082、2086、2095')
-	print('HTTPS 协议：443、2053、2083、2087、2096、8443')
-	print('在专业套餐及更高版本上，可以使用 WAF 规则 ID 100015 阻止除 80 和 443 之外的所有端口的请求')
-	print('80 和 443 端口是 Cloudflare Apps 能够使用的唯一端口')
-	print('80 和 443 端口是 Cloudflare Cache 能够使用的唯一端口')
-	print('请输入端口号（默认 2082） => ', end = '')
+	print(u'Cloudflare 支持的端口列表：')
+	print(u'HTTP 协议：80、8080、8880、2052、2082、2086、2095')
+	print(u'HTTPS 协议：443、2053、2083、2087、2096、8443')
+	print(u'在专业套餐及更高版本上，可以使用 WAF 规则 ID 100015 阻止除 80 和 443 之外的所有端口的请求')
+	print(u'80 和 443 端口是 Cloudflare Apps 能够使用的唯一端口')
+	print(u'80 和 443 端口是 Cloudflare Cache 能够使用的唯一端口')
+	print(u'请输入端口号（默认 2082） => ', end = '')
 	CFG_PORT = input()
 	if CFG_PORT == '':
 		CFG_PORT = 2082
@@ -112,42 +112,42 @@ def main():
 		try:
 			CFG_PORT = int(CFG_PORT)
 		except ValueError:
-			print('输入错误 ...')
+			print(u'输入错误 ...')
 		else:
 			defaultConf['inbounds'][0]['port'] = CFG_PORT
 	
-	print('正在生成用户连接 ID 中 ...', end = ' ')
+	print(u'正在生成用户连接 ID 中 ...', end = ' ')
 	defaultConf['inbounds'][0]['settings']['clients'][0]['id'] = str(uuid.uuid4())
-	print('完成 ...')
+	print(u'完成 ...')
 
-	print('正在更新软件源中 ...', end = ' ')
+	print(u'正在更新软件源中 ...', end = ' ')
 	if execute('apt update'):
-		print('完成 ...')
+		print(u'完成 ...')
 	else:
-		print('失败 ...')
+		print(u'失败 ...')
 		return
 	
-	print('正在安装依赖软件中 ...', end = ' ')
+	print(u'正在安装依赖软件中 ...', end = ' ')
 	if execute('apt install wget curl -y'):
 		print('完成 ...')
 	else:
 		print('失败 ...')
 		return
 	
-	print('正在下载核心程序中 ...', end = ' ')
+	print(u'正在下载核心程序中 ...', end = ' ')
 	if execute('wget -O v2ray-installer.sh https://install.direct/go.sh'):
 		print('成功，安装中 ...', end = ' ')
 
 		if execute('chmod +x v2ray-installer.sh') and execute('./v2ray-installer.sh --force'):
-			print('成功 ...')
+			print(u'成功 ...')
 		else:
-			print('失败 ...')
+			print(u'失败 ...')
 			return
 	else:
 		print('失败 ...')
 		return
 	
-	print('正在写入配置文件中 ...', end = ' ')
+	print(u'正在写入配置文件中 ...', end = ' ')
 	try:
 		open('/etc/v2ray/config.json', 'w').write(json.dumps(defaultConf))
 	except Exception as e:
@@ -156,19 +156,19 @@ def main():
 		except:
 			pass
 
-		print('失败 ...')
+		print(u'失败 ...')
 		return
 	
-	print('正在重启服务中 ...', end = ' ')
+	print(u'正在重启服务中 ...', end = ' ')
 	if execute('systemctl restart v2ray'):
 		print('成功 ...')
 	else:
 		print('失败 ...')
 	
-	print('ID：%s' % defaultConf['inbounds'][0]['settings']['clients'][0]['id'])
-	print('alterId：%i' % defaultConf['inbounds'][0]['settings']['clients'][0]['alterId'])
-	print('连接端口：%i' % defaultConf['inbounds'][0]['port'])
-	print('传输协议：%s' % defaultConf['inbounds'][0]['streamSettings']['network'])
+	print(u'ID：%s' % defaultConf['inbounds'][0]['settings']['clients'][0]['id'])
+	print(u'alterId：%i' % defaultConf['inbounds'][0]['settings']['clients'][0]['alterId'])
+	print(u'连接端口：%i' % defaultConf['inbounds'][0]['port'])
+	print(u'传输协议：%s' % defaultConf['inbounds'][0]['streamSettings']['network'])
 
 if __name__ == '__main__':
 	main()
